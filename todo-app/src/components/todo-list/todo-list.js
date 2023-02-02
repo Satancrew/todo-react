5fba2a2b9eb18b95cc2cb62cbcdaee1d0b3b63bb
Vladimir Orlov <satancrew.true@gmail.com>
2023-02-02 16:54:40 +0300

    refactor: refactoring structure

--- todo-app/src/components/todo-list/todo-list.js
+++ todo-app/src/components/todo-list/todo-list.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import TodoListItem from './todo-list-item';
+import TodoListItem from '../todo-list-item/todo-list-item';
+import './todo-list.css';
 
 const TodoList = ({ todos }) => {
   const elements = todos.map(item => {
     const { id, ...itemsProps } = item;
     return (
-      <li key={id}>
+      <li key={id} className='list-group-item'>
         <TodoListItem {...itemsProps} />
       </li>
     );
   });
-  return <ul>{elements}</ul>;
+  return <ul className='list-group todo-list'>{elements}</ul>;
 };
 
 export default TodoList;
